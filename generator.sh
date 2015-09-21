ea9269ce9ef0f44be01e534d64a2361972729b13
ricardoul <ricardoul@hotmail.com>
2015-09-21 16:45:58 -0300

    Agregado logica generador apodos

---
+++ generator.sh
@@ -0,0 +1,17 @@
+
+def hangoutService
+
+def nickGenerator(employeeId){
+
+	hangoutService.getFullName(employeeId).split(" ").collect{it.substring(0,1}) 
+}
+
+
+//Test
+
+checkJP(){
+
+	def e1 = new Employee(1, "J*** P*******", 18)
+	assert nickGenerator(1) == "JP"
+
+}
